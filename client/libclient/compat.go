f175791567393cab9052612a4c304047e720598e
Maxwell Krohn <themax@gmail.com>
2025-05-16 15:33:47 -0400

    cli: remove old version clash warning (#49)
    - close #48

--- client/libclient/compat.go
+++ client/libclient/compat.go
@@ -7,7 +7,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/charmbracelet/lipgloss"
 	"github.com/foks-proj/go-foks/lib/core"
 	"github.com/foks-proj/go-foks/proto/lcl"
 )
@@ -30,22 +29,6 @@ func MakeCheckProtoResHeader(errio IOStreamer) func(context.Context, lcl.Header)
 			return core.VersionNotSupportedError(
 				fmt.Sprintf("not supported: proto header version %d; please upgrade", v))
 		}
-		cv := h.V1().Semver
-		cmp := core.CurrentClientVersion.Cmp(cv)
-		var msg string
-		if cmp < 0 {
-			msg = fmt.Sprintf("Warning: CLI is older than FOKS agent; bad install? (%s < %s)", core.CurrentClientVersion, cv)
-		} else if cmp > 0 {
-			msg = fmt.Sprintf("Warning: CLI is newer than FOKS agent; try restarting the FOKS agent via `foks ctl restart` (%s > %s)", core.CurrentClientVersion, cv)
-		}
-		if msg == "" {
-			return nil
-		}
-		if errio.IsATTY() {
-			styl := lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000"))
-			msg = styl.Render(msg)
-		}
-		fmt.Fprintf(errio, "%s\n", msg)
 		return nil
 	}
 }
