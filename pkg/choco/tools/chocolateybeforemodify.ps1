4cfae646e51797ed04774260cde1790c0c593a61
Maxwell Krohn <themax@gmail.com>
2025-05-30 14:48:39 -0400

    choco: do a sleep after foks stop
    - choco upgrade might fail if the foks binary is still in use
    - for now, just wait 5 seconds for foks to exit; might want to do
      something smarter with `foks ctl stop` waiting

--- pkg/choco/tools/chocolateybeforemodify.ps1
+++ pkg/choco/tools/chocolateybeforemodify.ps1
@@ -2,3 +2,5 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Write-Host "Stopping foks..."
 & "$toolsDir\foks.exe" 'ctl' 'stop'
+Write-Host "Waiting 5 seconds for shutdown..."
+Start-Sleep -seconds 5
