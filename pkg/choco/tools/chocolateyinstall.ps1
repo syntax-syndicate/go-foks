0c68c3aa15f52c308eab723ab230c026589ee1d8
Maxwell Krohn <themax@gmail.com>
2025-06-23 15:30:24 -0400

    choco: version v0.0.21

--- pkg/choco/tools/chocolateyinstall.ps1
+++ pkg/choco/tools/chocolateyinstall.ps1
@@ -1,20 +1,17 @@
-$url64       = "https://github.com/foks-proj/go-foks/releases/download/v0.0.20/foks-v0.0.20-win-choco-amd64.zip"
-$url         = "https://github.com/foks-proj/go-foks/releases/download/v0.0.20/foks-v0.0.20-win-choco-x86.zip"
-$checksum    = "93a76d3dcbc6d47ba0cdaa5a32884d58159af8d04caec9b1fd0307e40507526a"
-$checksum64  = "6d9a663c2582d4a326fa513ae5ebbb2f03d8b1007be94c4e35771faa8bc0f097"
-$packageName = "foks"
 $toolsDir    = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage `
-  -PackageName   $packageName `
-  -FileType      'zip' `
-  -Url            $url `
-  -Url64bit       $url64 `
-  -Checksum       $checksum `
-  -ChecksumType   'sha256' `
-  -Checksum64     $checksum64 `
-  -ChecksumType64 'sha256' `
-  -UnzipLocation  $toolsDir
+$packageArgs = @{
+  packageName    = 'foks'
+  url            = 'https://github.com/foks-proj/go-foks/releases/download/v0.0.21/foks-v0.0.21-win-choco-x86.zip'
+  url64          = 'https://github.com/foks-proj/go-foks/releases/download/v0.0.21/foks-v0.0.21-win-choco-amd64.zip'
+  checksum64     = 'dcd28d09396c63ebcee05273abe6fbf75d7c9e1b7a55bebde7fbee2a7d9f47bf'
+  checksum       = 'ddca451bb1f49f0238d110f48f4d87f420dcf375db08275eb2793095662015db'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = $toolsDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
 
 # Need to copy the item over since we need to know the equivalent of os.Args[0]
 # inside the executable, and we lose that via the shimming process.
